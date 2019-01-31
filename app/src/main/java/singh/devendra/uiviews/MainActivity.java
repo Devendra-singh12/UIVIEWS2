5cf84ced73b97e21157931cb0560dd5954f967e8
devendra.singh_ca18@gla.ac.in <devendra.singh_mca18@gla.ac.in>
2019-01-31 11:22:36 +0530

    add java references for layout elements

--- app/src/main/java/singh/devendra/uiviews/MainActivity.java
+++ app/src/main/java/singh/devendra/uiviews/MainActivity.java
@@ -2,11 +2,32 @@ package singh.devendra.uiviews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    //   button, imageButton, checkBox, radioGroup,
+    //   radioButton, toggleButton, switch
+    //   radioGroup
+         // backward compatibility
+
+    private Button button;
+    private ImageButton imageButton;
+    private CheckBox checkBox;
+    private RadioGroup radioGroup;
+    private RadioButton radioButton1, radioButton2;
+    private Switch aSwitch;
+    private ToggleButton toggleButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
